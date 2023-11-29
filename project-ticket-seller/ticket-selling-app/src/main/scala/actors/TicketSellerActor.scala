95c096721fc101954bfdbf8fd12ca4a179024f8e
visveswaraiyaS <visveswaraiya.s@northeastern.edu>
2023-11-29 01:13:37 -0500

    Just TicketSellerActor file

--- project-ticket-seller/ticket-selling-app/src/main/scala/actors/TicketSellerActor.scala
+++ project-ticket-seller/ticket-selling-app/src/main/scala/actors/TicketSellerActor.scala
@@ -1,11 +1,10 @@
 package actors
 
-
 import actors.TicketSellerActor.TicketsState
 import akka.NotUsed
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.persistence.Persistence
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
@@ -15,139 +14,203 @@ import scala.util.{Failure, Success, Try}
 
 object TicketSellerActor {
   // Commands
-  sealed trait Command
+  sealed trait TicketSellerCommand
 
-  object Command {
-    case class BuyTickets(eventId: String, numOfTickets: Int, replyToCustomer: ActorRef[Response]) extends Command
-
-    case class GetTickets(event: String, replyToCustomer: ActorRef[Response]) extends Command
-
+  object TicketSellerCommand {
+    case class BuyTicket(eventId: String, numOfTickets: Int, customerID: String, replyToInventory: ActorRef[TicketSellerResponse]) extends TicketSellerCommand
+    case class CancelTicket(ticketID: String, replyToInventory: ActorRef[TicketSellerResponse]) extends TicketSellerCommand
     case class GetBookingReference()
   }
 
   // Events
-  sealed trait Event
-
-  case class TicketPurchased(ticketsState: TicketsState) extends Event
-
+  sealed trait TicketSellerEvent
+  case class TicketPurchased(ticketsState: TicketsState) extends TicketSellerEvent
+  case class TicketCancelled(changeStatus: String) extends TicketSellerEvent
 
   // Responses
-  sealed trait Response
-
-  object Response {
-    case class PurchaseResponse(tickets: TicketsState) extends Response
+  sealed trait TicketSellerResponse
+  object TicketSellerResponse {
+    case class PurchaseResponse(tickets: TicketsState) extends TicketSellerResponse
+    case class CancellationResponse(tickets: TicketsState) extends TicketSellerResponse
   }
 
   // State
-  case class TicketsState(ticketID: String, numberOfTickets: Int, ticketStatus: String, customer: String)
+  case class TicketsState(ticketID: String, eventID: String, numberOfTickets: Int, ticketStatus: String, customerID: String)
 
-  import Response._
-  import actors.TicketSellerActor.Command._
+  // available tickets
+  case class AvailableTickets(value: Int)
+  import TicketSellerActor.TicketSellerResponse._
+  import actors.TicketSellerActor.TicketSellerCommand._
 
   // Command Handler
   // create booking id in command handler rather than apply method
-  def commandHandler(context: ActorContext[Command]): (TicketsState, Command) => Effect[Event, TicketsState] = (state, command) => {
-    //    import eventManager.Response
+  def commandHandler(context: ActorContext[TicketSellerCommand]): (TicketsState, TicketSellerCommand) => Effect[TicketSellerEvent, TicketsState] = (state, command) => {
+
+    import PersistentEventManager.Response
+    import PersistentEventManager.Response._
+    import PersistentEventManager.Command._
+
     command match {
-      case BuyTickets(eventId, numOfTickets, customer) =>
-
-        // create id only here. coz apply will create id for everything.
-
+      case BuyTicket(eventId, numOfTickets, customerID, inventory) =>
         val id = state.ticketID
-        var availableTickets = 0
-        val responseHandler = context.spawn(Behaviors.receiveMessage[EventResponse] {
+        val eventManager = context.spawn(EventManagement(), "checkAvailability")
+        val eventManagerUpdate = context.spawn(EventManagement(), "updateAvailability")
+
+        val responseHandler = context.spawn(Behaviors.receiveMessage[Response] {
           case GetEventResponse(maybeEvent) =>
-            availableTickets = maybeEvent.get.id
+            println("getEventResponse: " + maybeEvent.get.venue)
+            val availableTickets = maybeEvent.get.maxTickets
+              if (numOfTickets > availableTickets) {
+                val ticketStatus = "Unsuccessful"
+                Effect
+                  .persist(TicketPurchased(TicketsState(id, eventId, numOfTickets, ticketStatus, customerID)))
+                  .thenReply(inventory)(newState => PurchaseResponse(newState))
+              } else {
+                eventManagerUpdate ! UpdateEvent(eventId, Some(10.3), Some("venue"), responseHandler)
+                val ticketStatus = "Successful"
+                Effect
+                  .persist(TicketPurchased(TicketsState(id, eventId, numOfTickets, ticketStatus, customerID)))
+                  .thenReply(inventory)(newState => PurchaseResponse(newState))
+              }
+            Behaviors.same
+          case EventUpdatedResponse(maybeEvent) =>
             Behaviors.same
         }, "replyHandler")
-        val eventManager = context.spawn(InventoryManager(event), "checkAvailability")
-        eventManager ! GetEvent(event, responseHandler)
+        eventManager ! GetEvent(eventId, responseHandler)
+        Effect.none
 
-        if (numOfTickets > availableTickets){
-          val ticketStatus = "Failed"
-          Effect
-            .persist(TicketPurchased(TicketsState(id, numOfTickets, ticketStatus, customer.get.id))
-            .thenReply(customer)(newState => PurchaseResponse(newState))
-        } else {
-          //create a command in Event Actor.
-          eventManager ! UpdateTickets(event, numOfTickets)
-          val ticketStatus = "Success"
-          Effect
-            .persist(TicketPurchased(TicketsState(id, numOfTickets, ticketStatus, customer.get.id)))
-            .thenReply(customer)(newState => PurchaseResponse(newState))
+
+
+      case CancelTicket(_, inventory) =>
+        val ticket_curr_status = state.ticketStatus
+        ticket_curr_status match {
+          case "Successful" =>
+            Effect
+              .persist(TicketCancelled("Cancelled"))
+              .thenReply(inventory)(newState => CancellationResponse(newState))
+          case "Unsuccessful" =>
+            Effect
+              .persist(TicketCancelled("Unsuccessful"))
+              .thenReply(inventory)(newState => CancellationResponse(newState))
+          case "Cancelled" =>
+            Effect
+              .persist(TicketCancelled("Cancelled"))
+              .thenReply(inventory)(newState => CancellationResponse(newState))
         }
-      case GetTickets(event, customer) => {
-      }
     }
   }
 
-  val eventHandler: (TicketsState, Event) => TicketsState = (state, event) =>
+  val eventHandler: (TicketsState, TicketSellerEvent) => TicketsState = (state, event) =>
     event match {
       case TicketPurchased(ticketsState) =>
         ticketsState
+      case TicketCancelled(change_status) =>
+        state.copy(ticketStatus = change_status)
     }
 
   // ticketID and eventID is passed from customer.
-  def apply(ticketID: String): Behavior[Command] = Behaviors.setup {
+  def apply(ticketID: String): Behavior[TicketSellerCommand] = Behaviors.setup {
     context => {
 
-      EventSourcedBehavior[Command, Event, TicketsState](
+      // EVENTSOURCEDBEHAVIOR - IDEA PERCEIVED FROM ROCK THE JVM
+      EventSourcedBehavior[TicketSellerCommand, TicketSellerEvent, TicketsState](
         persistenceId = PersistenceId.ofUniqueId(ticketID),
-        emptyState = TicketsState(ticketID, 0, "", ""),
+        emptyState = TicketsState(ticketID, "", 0, "", ""),
         commandHandler = commandHandler(context),
         eventHandler = eventHandler
       )
     }
   }
+}
 
+object InventoryManager {
+
+  import TicketSellerActor.TicketSellerCommand
+  import TicketSellerActor.TicketSellerCommand._
+  import TicketSellerActor.TicketSellerCommand._
+
+  // events
+  sealed trait Event
+
+  case class TicketsCreated(id: String) extends Event
+
+  // state
+  case class State(tickets: Map[String, ActorRef[TicketSellerCommand]])
+
+  def commandHandler(context: ActorContext[TicketSellerCommand]): (State, TicketSellerCommand) => Effect[Event, State] = (state, command) => {
+    println("state: " + state.tickets)
+    command match {
+      case buyCommand@BuyTicket(_, _, _, _) =>
+        val id = "BookingID-" + UUID.randomUUID().toString
+        val newPurchase = context.spawn(TicketSellerActor(id), id)
+        Effect
+          .persist(TicketsCreated(id))
+          .thenReply(newPurchase)(_ => buyCommand)
+      case cancelCommand@CancelTicket(ticketID, replyToInventory) =>
+        state.tickets.get(ticketID) match {
+          case Some(ticketActor) =>
+            Effect.reply(ticketActor)(cancelCommand)
+        }
+    }
+  }
+
+  def eventHandler(context: ActorContext[TicketSellerCommand]): (State, Event) => State = (state, event) => {
+    event match {
+      case TicketsCreated(id) =>
+        val tickets = context.child(id)
+          .getOrElse(context.spawn(TicketSellerActor(id), id))
+          .asInstanceOf[ActorRef[TicketSellerCommand]]
+        state.copy(state.tickets + (id -> tickets))
+
+    }
+  }
+
+  def apply(): Behavior[TicketSellerCommand] = Behaviors.setup {
+    context => {
+      EventSourcedBehavior[TicketSellerCommand, Event, State](
+        persistenceId = PersistenceId.ofUniqueId("InventoryManager"),
+        emptyState = State(Map()),
+        commandHandler = commandHandler(context),
+        eventHandler = eventHandler(context)
+      )
+    }
+  }
 }
 
 
-  object InventoryManager {
+object TicketSellerPlayGround {
+  import TicketSellerActor.TicketSellerCommand._
+  import TicketSellerActor.TicketSellerResponse._
+  import TicketSellerActor.TicketSellerResponse
+  import TicketSellerActor.TicketSellerCommand._
 
-    import TicketSellerActor.Command
-    import TicketSellerActor.Command._
-    import TicketSellerActor.Response._
 
-    // events
-    sealed trait Event
-    case class TicketsCreated(id: String) extends Event
+  def main(args: Array[String]): Unit = {
+    val rootBehavior: Behavior[NotUsed] = Behaviors.setup { context =>
 
-    // state
-    case class State(tickets: Map[String, ActorRef[Command]])
+      val inventory = context.spawn(InventoryManager(), "InventoryActor")
+//      val logger = context.log
 
-    def commandHandler(context: ActorContext[Command]): (State, Command) => Effect[Event, State] = (state, command) => {
-      command match {
-        case buyCommand @ BuyTickets(_, _, _) =>
-          val id = "BookingID-" + UUID.randomUUID().toString
-          val newTickets = context.spawn(TicketSellerActor(id), id)
-          Effect
-            .persist(TicketsCreated(id))
-            .thenReply(newTickets)(_ => buyCommand)
-      }
+      val responseHandlerInventory = context.spawn(Behaviors.receiveMessage[TicketSellerResponse]{
+        case PurchaseResponse(ticket) =>
+          val ticketStatus = ticket.ticketStatus
+          println(ticketStatus)
+          Behaviors.same
+        case CancellationResponse(ticket) =>
+          println("purchased customer: " + ticket.customerID)
+          println("new ticket status: " + ticket.ticketStatus)
+          Behaviors.same
+      }, "replyHandler")
+
+//      val logger = context.log
+
+
+      //       test 1
+      inventory ! BuyTicket("eventID001", 10, "customerID1", responseHandlerInventory)
+//      inventory ! CancelTicket("BookingID-89c1b68f-8563-4c31-8fdb-e78a44a1c3bd", responseHandlerInventory )
+      Behaviors.empty
     }
-
-    def eventHandler(context: ActorContext[Command]): (State, Event) => State = (state, event) => {
-      event match {
-        case TicketsCreated(id) =>
-          val tickets = context.child(id)
-          .getOrElse(context.spawn(TicketSellerActor(id), id))
-          .asInstanceOf[ActorRef[Command]]
-          state.copy(state.tickets + (id -> tickets))
-
-      }
-    }
-
-    def apply(): Behavior[Command] = Behaviors.setup {
-      context => {
-        EventSourcedBehavior[Command, Event, State](
-          persistenceId = PersistenceId.ofUniqueId("InventoryManager"),
-          emptyState = State(Map()),
-          commandHandler = commandHandler(context),
-          eventHandler = eventHandler(context)
-        )
-      }
-    }
-
-
+    val system = ActorSystem(rootBehavior, "TicketSellerDemo")
   }
+}
+
